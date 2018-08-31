cd4a9d49961b2c50b028786ff9e4f772c2fddc79
akintola olalekan <akinlekan@gmail.com>
2018-08-31 10:32:24 +0100

    Wrapped up auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { logoutUser, setCurrentUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 import './App.css';
@@ -8,6 +11,27 @@ import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+//check for token
+if (localStorage.jwtToken) {
+  //set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  //decode token and get user info
+  const decoded = jwt_decode(localStorage.jwtToken);
+  //set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  //check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
 
 
 class App extends Component {
--- routes/api/users.js
+++ routes/api/users.js
@@ -19,9 +19,9 @@ const User = require('../../models/User');
 //@access Public
 router.post('/register', (req, res) => {
 
-  const {errors, isValid} = validateRegisterInput(req.body);
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-  if(!isValid){
+  if (!isValid) {
     return res.status(400).json(errors);
   }
 
@@ -29,37 +29,37 @@ router.post('/register', (req, res) => {
   User.findOne({
     email: req.body.email
   })
-  .then(user => {
-    if(user){
-      errors.email = 'Email already exists'
-      return res.status(400).json(errors)
-    } else {
+    .then(user => {
+      if (user) {
+        errors.email = 'Email already exists'
+        return res.status(400).json(errors)
+      } else {
 
-      const avatar = gravatar.url(req.body.email, {
-        s: '200',
-        r: 'pg',
-        d: 'mm'
-      });
+        const avatar = gravatar.url(req.body.email, {
+          s: '200',
+          r: 'pg',
+          d: 'mm'
+        });
 
-      const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        avatar,
-        password: req.body.password
-      })
-
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if(err) throw err;
-          newUser.password = hash;
-          newUser.save()
-          .then(user => res.json(user))
-          .catch(err => console.log(err))
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
         })
-      })
-    }
-  })
-  .catch();
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err))
+          })
+        })
+      }
+    })
+    .catch();
 });
 
 //@route  Get api/users/login
@@ -80,44 +80,44 @@ router.post('/login', (req, res) => {
   User.findOne({
     email
   })
-  .then(user => {
-    //Check for user
-    if(!user){
-      errors.email = 'User not found'
-      return res.status(404).json(errors)
-    } 
-    //Check password
-    bcrypt.compare(password, user.password)
-    .then(isMatch => {
-      if(isMatch){
-        //User match
-        const payload = {id: user.id, name: user.name, avatar: user.avatar} //Create JWT Payload
-
-        //Sign token
-        jwt.sign(
-          payload, 
-          keys.secretKey, 
-          { expiresIn: 3600}, 
-          (e, token) => {
-            res.json({
-              success: true,
-              token: 'Bearer ' + token 
-            })
-        });
-      }else {
-        errors.password = 'Password incorrect';
-        return res.status(400).json({errors})
+    .then(user => {
+      //Check for user
+      if (!user) {
+        errors.email = 'User not found'
+        return res.status(404).json(errors)
       }
+      //Check password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            //User match
+            const payload = { id: user.id, name: user.name, avatar: user.avatar } //Create JWT Payload
+
+            //Sign token
+            jwt.sign(
+              payload,
+              keys.secretKey,
+              { expiresIn: 3600 },
+              (e, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                })
+              });
+          } else {
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors)
+          }
+        })
     })
-  })
-  .catch(err => console.log(err))
+    .catch(err => console.log(err))
 })
 
 //@route  Get api/users/current
 //@desc Returns current user route
 //@access Private
 
-router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
   res.json({
     id: req.user.id,
     name: req.user.name,
